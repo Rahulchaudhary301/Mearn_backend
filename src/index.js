e0a0cd7e32e623f3a29063a50321a5f1d38aad3a
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2023-04-14 02:50:22 +0530

    rahul

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ const app=express()
 const router=require("./Router/router")
 const {default:mongoose}=require("mongoose")
 const cors=require('cors')
-const DB=process.env.DATABASE
+const DB=process.env.MONGO_URI
+
+
 
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb+srv://RahulChaudhary:Rahul321@cluster1.42h1ws9.mongodb.net/MEARN_UserRegistration",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+mongoose.connect("mongodb+srv://RahulChaudhary:Rahul321@cluster1.42h1ws9.mongodb.net/MEARN_UserRegistration?retryWrites=true&w=majority")
 .then(()=>console.log("MOngoDb Connected")) 
 .catch((err)=>console.log(err.message))
 
